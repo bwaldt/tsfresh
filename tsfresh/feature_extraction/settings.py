261a09ed2a526bfd6ca1e79732c910eff0b2d2f7
bwaldt <jbrooke@seas.upenn.edu>
2020-10-01 18:25:04 -0400

    Update settings.py

--- tsfresh/feature_extraction/settings.py
+++ tsfresh/feature_extraction/settings.py
@@ -121,7 +121,7 @@ class ComprehensiveFCParameters(dict):
             "agg_autocorrelation": [{"f_agg": s, "maxlag": 40} for s in ["mean", "median", "var"]],
             "partial_autocorrelation": [{"lag": lag} for lag in range(10)],
             "number_cwt_peaks": [{"n": n} for n in [1, 5]],
-            "number_peaks": [{"n": n} for n in [1, 3, 5, 10, 50]],
+            "number_peaks": [{"n": n} for n in [1, 3, 5, 10, 15, 20]],
             "binned_entropy": [{"max_bins": max_bins} for max_bins in [10]],
             "index_mass_quantile": [{"q": q} for q in [.1, .2, .3, .4, .6, .7, .8, .9]],
             "cwt_coefficients": [{"widths": width, "coeff": coeff, "w": w} for
